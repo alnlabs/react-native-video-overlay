5d874ce3c491e21ced2c062e8a774aee79409aac
ALN Labs <alnlabs1@gmail.com>
2025-06-26 11:42:39 +0530

    bug fixes

--- android/src/main/java/com/alnlabs/videooverlay/VideoOverlayPackage.kt
+++ android/src/main/java/com/alnlabs/videooverlay/VideoOverlayPackage.kt
@@ -3,6 +3,7 @@ package com.alnlabs.videooverlay
 import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.uimanager.ViewManager
 
@@ -16,7 +17,22 @@ class VideoOverlayPackage : TurboReactPackage() {
   }
 
   override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return TurboReactPackage.getReactModuleInfoProviderForClass(VideoOverlayModule::class.java)
+    return ReactModuleInfoProvider {
+      val moduleInfoMap = HashMap<String, ReactModuleInfo>()
+      val moduleName = VideoOverlayModule.NAME
+
+      val moduleInfo = ReactModuleInfo(
+        moduleName,
+        VideoOverlayModule::class.java.name,
+        /* canOverrideExistingModule */ false,
+        /* needsEagerInit */ false,
+        /* hasConstants */ true,
+        /* isCxxModule */ false,
+        /* isTurboModule */ true
+      )
+      moduleInfoMap[moduleName] = moduleInfo
+      moduleInfoMap
+    }
   }
 
   override fun createViewManagers(context: ReactApplicationContext): List<ViewManager<*, *>> {
